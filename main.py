3d2d8a4ee2ee1ac0233ab9b88f47642643f96656
ghdwpaks <hjm7904@gmail.com>
2021-03-07 16:49:52 +0900

    eng word test program part 3 'get input words from user ans use check right ans'

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import csv
 import os
 print("\n\n\n\n")
-path = 'word_table.txt'
-f = open(path,'rt',encoding='UTF8')
-line = f.readlines()
-eng_words= str(line[0]).split(',')
-kor_words = str(line[1]).split(',')
 '''
 print(eng_words)
 print(kor_words)
 '''
 
-def get_int_and(s):
+def get_int(s):
     print()
     p = False
     u = 0
@@ -41,20 +36,18 @@ def ans_split(s) :
 
 
 
-def check_right(userans,pronum,eng_or_kor) :
+def check_right(userans,  pronum ,words,show_ans=0) :
+    #show_ans 가 1 이면 바로 보여주고 0이면 안보여주기
     '''
     print("userans:{}".format(userans))
     print("pronum:{}".format(pronum))
     print("eng_or_kor:{}".format(eng_or_kor))
     '''
     #print("userans :{}".format(userans))
-    userans = userans.split(" ")
+    userans = (userans).strip().split(" ")
     ps = False#맞으면 true, 틀리면 False
     right_ans_count = 0
-    if eng_or_kor == 0 : #답이 한글
-        right_ans = (kor_words[pronum]).strip().split(" ")
-    elif eng_or_kor == 1 : #답이 영어
-        right_ans = (eng_words[pronum]).strip().split(" ")
+    right_ans = (words[pronum]).strip().split(" ")
     '''
     print('right_ans:{}'.format(right_ans))
     print("len(right_ans):{}".format(len(right_ans)))
@@ -84,20 +77,81 @@ def check_right(userans,pronum,eng_or_kor) :
                 
                 right_ans_count += 1
                 #print("right_ans_count:{}".format(right_ans_count))
+                
         except :
             ps = False
-            print("오류가 발생했습니다.")
+            #print("오류가 발생했습니다.")
             break
     if right_ans_count >= len(right_ans) and len(userans) == len(right_ans) :
         ps = True
+        if show_ans == 1 :
+            print("맞았습니다!")
     else :
+        if show_ans == 1 :
+            print("정답은 {}입니다.".format(words[pronum].strip()))
         ps = False
     return ps
+
+def test_words(path,eng_or_kor,show_ans=0) :
+    wrong_ans = []
+    correct_count = 0
+    f = open(path,'rt',encoding='UTF8')
+    line = f.readlines()
+    eng_words= str(line[0]).split(',')
+    kor_words = str(line[1]).split(',')
+    right_words = []
+    show_words = []
+    if eng_or_kor == 1 : #답이 한글
+        right_words = kor_words #이 시점에서 right_words는 문자열 상태이다
+        show_words = eng_words
+        #단어'들'이 들어있는것은 kor_words가 된다.
+    elif eng_or_kor == 0 : #답이 영어
+        right_words = eng_words
+        show_words = kor_words
+
+    for i in range(0,len(right_words)) :
+        userans_word = input("{}\n>>".format(show_words[i]))
+        if check_right(userans_word,i,right_words,show_ans) :
+            correct_count += 1
+        else :
+            wrong_ans.append(i)
+    print("맞은 갯수는 {}개 입니다!".format(correct_count))
+    print("틀린문제는",end="")
+    if len(wrong_ans) <= 0 :
+        print(" 없습니다.")
+    else :
+        print()
+        for i in range(0,len(wrong_ans)) :
+            print("{}번의 '{}'".format(wrong_ans[i]+1,right_words[wrong_ans[i]].strip()))
+        print("들이 있습니다.")
+
+    
+        
+
+
 
 
 
 print("Hello world!")
-res = check_right('      ...에 따르면      '.strip(),2,0)
-print("res : ",res)
+while True :
+    
+    path = ''
+    print("현재 시험 가능 과목은 1과 입니다.")
+    userans_check_unit = get_int("몇과를 시험볼건지 정해주세요. >>")
+
+    userans_check_language = get_int("영어를 입력할거면 0, 한글을 입력할거면 1. >>")
+    show_ans = get_int("틀린 답을 바로 공개할거면 1, 아니면 0. >>")
+    if userans_check_language == 1 or userans_check_language == 0 :
+        if userans_check_unit == 1 :
+            path = './words_file/word_table1.txt'
+            test_words(path,userans_check_language,show_ans)
+        else :
+            continue
+    else :
+        continue
+
+    
+
+
 
 
